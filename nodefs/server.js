300341de38ffb4c5d4827811a70713c260ae73dc
Lamba Alexandru <roshkattu94@gmail.com>
2014-08-18 21:00:17 +0300

    Working version of brackets 0.43 in browser

---
+++ nodefs/server.js
@@ -0,0 +1,340 @@
+/**
+ * Node File System API Server
+ * ---------------------------
+ * Uses Socket.IO to make calls to different commands
+ * on the host system that are required by Brackets
+ */
+
+var fs = require('fs'),
+    app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+    /**
+     * Constants
+     */
+
+    var _SERVER_PORT = 3000,
+        Errors = {
+            EmptyPath: "Empty path provided"
+        };
+
+    /**
+     * Variables
+     */
+
+    var _connections = []; //Keep track of nodejs connections
+    var _socket = null; //Keep a copy of the current socket
+    var _basePath = "/var/www/";
+
+
+
+    /**
+     * Socket server logic
+     */
+    io.on('connection', function(socket){
+
+        /**
+         * Push our socket connection within the _connections array
+         */
+        _connections.push(socket);
+        _socket = socket;
+
+        //Show message that our socket has been added to the connections pool
+        console.log("A new connection has been established with: "+socket.id);
+
+        /**
+         * Wait for a command from the client
+         * @param {String} command - command name to be executed
+         * @param {Object} data - json object having the function data
+         */
+        socket.on('execCommand', function(command, data, callback){
+            //Switch the command, execute it and send back the return
+            switch(command){
+                    //Call mkdir
+                    case "mkdir":
+                        mkdir(data.path, function(err, stats){
+                            callback(err, stats);
+                        });
+                    break;
+
+                    //call readfile
+                    case "readfile":
+                        readfile(data.path, data.otpions, function(err, data, stats){
+                            callback(err, data, stats);
+                        });
+                    break;
+
+                    //call stat
+                    case "stat":
+                        stat(data.path, function(err, stats){
+                            callback(err, stats);
+                        })
+                    break;
+
+                    //exists call
+                    case "exists":
+                        exists(data.path, function(err, exists){
+                            callback(err, exists);
+                        });
+                    break;
+
+                    //readdir call
+                    case "readdir":
+                        readdir(data.path, function(err, _files, _file_stats){
+                            callback(err, _files, _file_stats);
+                        });
+                    break;
+
+                    //rename call
+                    case "rename":
+                        rename(data.oldPath, data.newPath, function(response){
+                            callback(response);
+                        });
+                    break;
+
+                    //writefile call
+                    case "writefile":
+                        writefile(data.path, data.data, data.otpions, function(err, stats, created){
+                            callback(err, stats, created);
+                        });
+                    break;
+
+                    //unlink call
+                    case "unlink":
+                        unlink(data.path, function(response){
+                            callback(response);
+                        });
+                    break;
+            }
+        });
+    });
+
+
+
+
+
+    /** ----------------------- FS COMMANDS  ----------------------- */
+
+    /**
+     * exists(path, callback)
+     */
+    function exists(path, callback){
+        //Check if the file exists
+        fs.exists(path, function(exists){
+            callback(null,exists); 
+        });
+    };
+
+    /**
+     * stat(path,callback)
+     * Return stats of a file
+     */
+    function stat(path, callback){
+        //Get file stats
+        fs.stat(path, function(err, _stats){
+            var _this_file_stats = {
+                isFile: _stats.isFile(),
+                mtime: _stats.mtime,
+                size: _stats.size,
+                realPath: path,
+                hash: _stats.mtime.getTime()
+            }
+            console.log(_this_file_stats);
+            callback(err, _this_file_stats)
+        });
+    }
+
+    /**
+     * readdir(path, callback)
+     */
+    function readdir(path,callback){
+        //Read the directory and return the files details
+        fs.readdir(path, function(err,files){
+            if(null === err){
+                //Recurse the file and return file name + file stats
+                var _files = [];
+                var _file_stats = [];
+                var _length = files.length;
+                //Check to see if there are any contents
+                if(!_length){
+                    callback(null, [], []);
+                    return;
+                }
+                //Iterate files
+                files.forEach(function(file, pos){
+                    /**
+                     * Append the current path to the file. Since we're only using this on linux the DIRECTORY_SEPARATOR
+                     * will always be "/"
+                     */
+                    file = path + file;
+                    var _stats = fs.statSync(file);
+                    //Push the stats within the return object
+                    
+                    var _this_file_stats = {
+                        isFile: _stats.isFile(),
+                        mtime: _stats.mtime,
+                        size: _stats.size,
+                        realPath: file,
+                        hash: _stats.mtime.getTime()
+                    }
+                    console.log(_this_file_stats);
+                    _file_stats[pos] = _this_file_stats;
+                });
+                
+                callback(err, files, _file_stats);
+                return;
+            } else {
+                //Return the error of reading the directory
+                callback(err);
+                return;
+            }
+        });
+    };
+
+    /**
+     * mkdir(path, mode, callback)
+     */
+    function mkdir(path, mode, callback){
+        //Check typeof mode to see if the mode has been provided or not
+        if (typeof mode === "function") {
+            callback = mode;
+            mode = parseInt("0755", 8);
+        }
+        //Create the directory
+        fs.mkdir(path, mode, function(err){
+           if(null === err){
+               //Get file stats
+                fs.stat(path, function(err, _stats){
+                    if(err === null){
+                        //Return file stats
+                        var _this_file_stats = {
+                            isFile: _stats.isFile(),
+                            mtime: _stats.mtime,
+                            size: _stats.size,
+                            realPath: path,
+                            hash: _stats.mtime.getTime()
+                        }
+                        console.log(_this_file_stats);
+                        callback(err, _this_file_stats);
+                    } else {
+                        //Return error
+                        callback(err);
+                    }
+                });
+           } else {
+               callback(err);//Return the error
+           }
+        });
+    };
+
+    /**
+     * readFile(path, options, callback)
+     */
+    function readfile(path, options, callback){
+        //Read the file and get the data
+        fs.readFile(path, 'utf8', function(err, data){
+            if(null === err){
+                //Return data
+                fs.stat(path, function(serr, _stats){
+                    if(serr === null){
+                        //Return file stats
+                        var _this_file_stats = {
+                            isFile: _stats.isFile(),
+                            mtime: _stats.mtime,
+                            size: _stats.size,
+                            realPath: path,
+                            hash: _stats.mtime.getTime()
+                        }
+                        console.log(_this_file_stats);
+                        callback(err, data, _this_file_stats);
+                    } else {
+                        //Return error
+                        callback(err, data, null);
+                    }
+                });
+            } else {
+                //Return error
+                callback(err, null, null);
+            }
+        });
+    }
+
+    /**
+     * rename(oldPath, newPath, callback)
+     */
+    function rename(oldPath, newPath, callback){
+        fs.rename(oldPath, newPath, function(err){
+            if(null === exc){
+                //Console log
+                callback(null);
+            } else {
+                //Return error if any
+                callback(err);
+            }
+        });
+    }
+
+    /**
+     * writeFile(path, data, [options], callback)
+     */
+    function writefile(path, data, options, callback){
+        if("function" === typeof options){
+            callback = options; //Get the callback function if the options parameter isn't set
+        }
+        fs.writeFile(path, data, options, function(err){
+             if(null === err){
+                 //Return file stats
+                 fs.stat(path, function(serr, _stats){
+                    if(serr === null){
+                        //Return file stats
+                        var _this_file_stats = {
+                            isFile: _stats.isFile(),
+                            mtime: _stats.mtime,
+                            size: _stats.size,
+                            realPath: path,
+                            hash: _stats.mtime.getTime()
+                        }
+                        console.log(_this_file_stats);
+                        callback(serr, stats, _this_file_stats);
+                    } else {
+                        //Return error
+                        callback(serr, null, true);
+                    }
+                });
+             } else {
+                 //Return error
+                 callback(err, null, false);
+             }
+        });
+    }
+
+    /**
+     * unlink(path, callback)
+     */
+    function unlink(path, callback){
+        fs.unlink(path, function(err){
+            if(null === err){
+                //Console log
+                callback(null);
+            } else {
+                //Return error
+                callback(err);
+            }
+        });
+    }
+
+    /** ----------------------- FS COMMANDS END -------------------- */
+
+
+
+
+    /**
+     * Start the server on our defined _EXPRESS_PORT
+     *
+     * @param _SERVER_PORT {Integer}
+     * Server port
+     */
+    http.listen(_SERVER_PORT, function(){
+        console.log("Your NodeFS Server is running on port "+ _SERVER_PORT);
+    });
---
+++ src/project/WorkingSetSort.js
@@ -0,0 +1,363 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, $, window */
+
+/**
+ * Manages the workingSet sort methods.
+ */
+define(function (require, exports, module) {
+    "use strict";
+    
+    var Commands                = require("command/Commands"),
+        CommandManager          = require("command/CommandManager"),
+        DocumentManager         = require("document/DocumentManager"),
+        PreferencesManager      = require("preferences/PreferencesManager"),
+        FileUtils               = require("file/FileUtils"),
+        AppInit                 = require("utils/AppInit"),
+        Strings                 = require("strings");
+    
+    var defaultPrefs = {
+        currentSort:   Commands.SORT_WORKINGSET_BY_ADDED,
+        automaticSort: false
+    };
+    
+    /**
+     * @private
+     * @type {Array.<Sort>}
+     */
+    var _sorts = [];
+    
+    /**
+     * @private
+     * @type {Sort}
+     */
+    var _currentSort = null;
+    
+    /**
+     * @private
+     * @type {boolean}
+     */
+    var _automaticSort = false;
+    
+    /**
+     * @private
+     * @type {boolean}
+     * Used to know when to do the automatic sort for MRU order.
+     */
+    var _openedDocument = false;
+    
+    /**
+     * Retrieves a Sort object by id
+     * @param {(string|Command)} command A command ID or a command object.
+     * @return {?Sort}
+     */
+    function get(command) {
+        var commandID;
+        if (!command) {
+            console.error("Attempting to get a Sort method with a missing required parameter: command");
+            return;
+        }
+        
+        if (typeof command === "string") {
+            commandID = command;
+        } else {
+            commandID = command.getID();
+        }
+        return _sorts[commandID];
+    }
+    
+    /**
+     * @return {boolean} Enabled state of Automatic Sort.
+     */
+    function getAutomatic() {
+        return _automaticSort;
+    }
+    
+    /**
+     * @private
+     * Removes the sort DocumentManager listeners.
+     */
+    function _removeListeners() {
+        $(DocumentManager).off(".sort");
+    }
+    
+    /**
+     * Enables/Disables Automatic Sort depending on the value.
+     * @param {boolean} enable True to enable, false to disable.
+     */
+    function setAutomatic(enable) {
+        _automaticSort = enable;
+        PreferencesManager.setViewState("automaticSort", enable);
+        CommandManager.get(Commands.SORT_WORKINGSET_AUTO).setChecked(enable);
+        _currentSort.setChecked(enable);
+        
+        if (enable) {
+            _currentSort.sort();
+        } else {
+            _removeListeners();
+        }
+    }
+    
+    /**
+     * @private
+     * Adds the current sort DocumentManager listeners.
+     */
+    function _addListeners() {
+        if (_automaticSort && _currentSort && _currentSort.getEvents()) {
+            $(DocumentManager)
+                .on(_currentSort.getEvents(), function () {
+                    _currentSort.sort();
+                })
+                .on("workingSetDisableAutoSorting.sort", function () {
+                    setAutomatic(false);
+                });
+        }
+    }
+    
+    
+    /**
+     * @private
+     * Sets the current sort method and checks it on the context menu.
+     * @param {Sort} newSort
+     */
+    function _setCurrentSort(newSort) {
+        var command;
+        if (_currentSort !== newSort) {
+            if (_currentSort !== null) {
+                _currentSort.setChecked(false);
+            }
+            if (_automaticSort) {
+                newSort.setChecked(true);
+            }
+            
+            CommandManager.get(Commands.SORT_WORKINGSET_AUTO).setEnabled(!!newSort.getEvents());
+            PreferencesManager.setViewState("currentSort", newSort.getCommandID());
+            _currentSort = newSort;
+        }
+    }
+    
+    
+    /**
+     * @constructor
+     * @private
+     *
+     * @param {string} commandID A valid command identifier.
+     * @param {function(File, File): number} compareFn A valid sort
+     *      function (see register for a longer explanation).
+     * @param {string} events Space-separated DocumentManager possible events
+     *      ending with ".sort".
+     */
+    function Sort(commandID, compareFn, events, automaticFn) {
+        this._commandID = commandID;
+        this._compareFn = compareFn;
+        this._events    = events;
+    }
+    
+    /**
+     * The Command ID
+     * @return {string}
+     */
+    Sort.prototype.getCommandID = function () {
+        return this._commandID;
+    };
+    
+    /**
+     * The compare function
+     * @return {function(File, File): number}
+     */
+    Sort.prototype.getCompareFn = function () {
+        return this._compareFn;
+    };
+    
+    /**
+     * The DocumentManager events
+     * @return {string}
+     */
+    Sort.prototype.getEvents = function () {
+        return this._events;
+    };
+    
+    /**
+     * Checks/Unchecks the command which will show a check in the menu
+     * @param {boolean} value
+     */
+    Sort.prototype.setChecked = function (value) {
+        var command = CommandManager.get(this._commandID);
+        if (command) {
+            command.setChecked(value);
+        }
+    };
+
+    /**
+     * Performs the sort and makes it the current sort method.
+     */
+    Sort.prototype.execute = function () {
+        _setCurrentSort(this);
+        this.sort();
+    };
+    
+    /**
+     * Only performs the working set sort if this is the current sort.
+     */
+    Sort.prototype.sort = function () {
+        if (_currentSort === this) {
+            _removeListeners();
+            DocumentManager.sortWorkingSet(this._compareFn);
+            _addListeners();
+        }
+    };
+    
+    
+    /**
+     * Registers a working set sort method.
+     * @param {(string|Command)} command A command ID or a command object
+     * @param {function(File, File): number} compareFn The function that
+     *      will be used inside JavaScript's sort function. The return a value
+     *      should be >0 (sort a to a lower index than b), =0 (leaves a and b
+     *      unchanged with respect to each other) or <0 (sort b to a lower index
+     *      than a) and must always returns the same value when given a specific
+     *      pair of elements a and b as its two arguments. Documentation at:
+     *      https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Array/sort
+     * @param {?string} events One or more space-separated event types that
+     *      DocumentManger uses. Each event passed will trigger the automatic
+     *      sort. If no events are passed, the automatic sort will be disabled
+     *      for that sort method.
+     * @return {?Sort}
+     */
+    function register(command, compareFn, events) {
+        var commandID = "";
+        
+        if (!command || !compareFn) {
+            console.log("Attempting to register a Sort method with a missing required parameter: command or compare function");
+            return;
+        }
+        if (typeof command === "string") {
+            commandID = command;
+        } else {
+            commandID = command.getID();
+        }
+        
+        if (_sorts[commandID]) {
+            console.log("Attempting to register an already-registered Sort method: " + command);
+            return;
+        }
+        
+        // Adds ".sort" to the end of each event to make them specific for the automatic sort.
+        if (events) {
+            events = events.split(" ");
+            events.forEach(function (event, index) {
+                events[index] = events[index].trim() + ".sort";
+            });
+            events = events.join(" ");
+        }
+        
+        var sort = new Sort(commandID, compareFn, events);
+        _sorts[commandID] = sort;
+        return sort;
+    }
+    
+    
+    /** Command Handlers */
+    function _handleSortWorkingSetByAdded() {
+        get(Commands.SORT_WORKINGSET_BY_ADDED).execute();
+    }
+    
+    function _handleSortWorkingSetByName() {
+        get(Commands.SORT_WORKINGSET_BY_NAME).execute();
+    }
+    
+    function _handleSortWorkingSetByType() {
+        get(Commands.SORT_WORKINGSET_BY_TYPE).execute();
+    }
+    
+    function _handleAutomaticSort() {
+        setAutomatic(!getAutomatic());
+    }
+    
+    
+    // Register Sort Methods
+    register(
+        Commands.SORT_WORKINGSET_BY_ADDED,
+        function (file1, file2) {
+            var index1 = DocumentManager.findInWorkingSetAddedOrder(file1.fullPath),
+                index2 = DocumentManager.findInWorkingSetAddedOrder(file2.fullPath);
+            
+            return index1 - index2;
+        },
+        "workingSetAdd workingSetAddList"
+    );
+    register(
+        Commands.SORT_WORKINGSET_BY_NAME,
+        function (file1, file2) {
+            return FileUtils.compareFilenames(file1.name, file2.name, false);
+        },
+        "workingSetAdd workingSetAddList"
+    );
+    register(
+        Commands.SORT_WORKINGSET_BY_TYPE,
+        function (file1, file2) {
+            return FileUtils.compareFilenames(file1.name, file2.name, true);
+        },
+        "workingSetAdd workingSetAddList"
+    );
+    
+    
+    // Register Command Handlers
+    CommandManager.register(Strings.CMD_SORT_WORKINGSET_BY_ADDED, Commands.SORT_WORKINGSET_BY_ADDED, _handleSortWorkingSetByAdded);
+    CommandManager.register(Strings.CMD_SORT_WORKINGSET_BY_NAME,  Commands.SORT_WORKINGSET_BY_NAME,  _handleSortWorkingSetByName);
+    CommandManager.register(Strings.CMD_SORT_WORKINGSET_BY_TYPE,  Commands.SORT_WORKINGSET_BY_TYPE,  _handleSortWorkingSetByType);
+    CommandManager.register(Strings.CMD_SORT_WORKINGSET_AUTO,     Commands.SORT_WORKINGSET_AUTO,     _handleAutomaticSort);
+    
+    
+    // Initialize default values for sorting preferences
+    PreferencesManager.stateManager.definePreference("currentSort", "string", Commands.SORT_WORKINGSET_BY_ADDED);
+    PreferencesManager.stateManager.definePreference("automaticSort", "boolean", false);
+
+    PreferencesManager.convertPreferences(module, {"currentSort": "user", "automaticSort": "user"}, true);
+    
+    // Initialize items dependent on extensions/workingSet
+    AppInit.appReady(function () {
+        var curSort  = get(PreferencesManager.getViewState("currentSort")),
+            autoSort = PreferencesManager.getViewState("automaticSort");
+        
+        if (curSort) {
+            _setCurrentSort(curSort);
+        }
+        if (autoSort) {
+            setAutomatic(true);
+        }
+        if (curSort && autoSort) {
+            curSort.sort();
+        }
+    });
+    
+    
+    // Define public API
+    exports.register        = register;
+    exports.get             = get;
+    exports.getAutomatic    = getAutomatic;
+    exports.setAutomatic    = setAutomatic;
+});
